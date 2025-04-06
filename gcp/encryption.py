0645d8ffd98185eed6e5cce903254562b5e29949
icevirm <icevirm13@gmail.com>
2025-04-06 15:40:03 +0200

    Added location output for bucket and key

--- gcp/encryption.py
+++ gcp/encryption.py
@@ -8,19 +8,30 @@ console = Console()
 client = storage.Client()
 
 
-def get_bucket_encryption(bucket: str) -> dict:
+def get_bucket(bucket: str) -> dict:
     '''
-    Gets encryption configuration from the bucket
-
+    Gets bucket configuration
+    
     Args: (str) bucket - the name of the bucket to scan
-    Returns: (dict) Encryption configuration from response
+    Returns: (dict) Bucket object
     '''
     try:
         response = client.get_bucket(bucket)
 
-        return response.default_kms_key_name
+        return response
     except Exception as e:
         print(e)
+
+
+def parse_key(key: str) -> str:
+    '''
+    Returns the location of the encryption key
+    
+    Args: (str) key - encryption key used to encrypt the bucket
+    Returns: (str) - key location parsed from the name, e.g.
+        projects/project-1234/locations/europe-west1/keyRings/storage-eu/cryptoKeys/buckets-eu -> europe-west1
+    '''
+    return key.split('/')[3]
 
 
 def encryption_configuration(buckets: list) -> None:
@@ -31,24 +42,32 @@ def encryption_configuration(buckets: list) -> None:
     Args: (list) buckets - list of buckets in the current account
     Returns: None
     '''
-    table = Table(title="GCS Buckets Security Scan Results")
-    table.add_column("Bucket Name", style="cyan", justify="left")
-    table.add_column("Encryption Type", style="magenta", justify="center")
-    table.add_column("Encryption Key", style="magenta", justify="center")
+    table = Table(title='GCS Buckets Security Scan Results')
+    table.add_column('Bucket Name', style='cyan', justify='left')
+    table.add_column('Encryption Type', style='magenta', justify='center')
+    table.add_column('Encryption Key', style='magenta', justify='center')
+    table.add_column('Key Location', style='magenta', justify='center')
+    table.add_column('Bucket Location', style='magenta', justify='center')
 
-    for bucket in tqdm(buckets, desc="Scanning Buckets", unit="bucket"):
-        default_kms_key_name = get_bucket_encryption(bucket)
-        encryption_algorithm = "AES-256"
+    for bucket in tqdm(buckets, desc='Scanning Buckets', unit='bucket'):
+        bucket_object = get_bucket(bucket)
+        default_kms_key_name = bucket_object.default_kms_key_name
+        location = bucket_object.location.lower()
+        encryption_algorithm = 'AES-256'
         
-        if default_kms_key_name:    
-            encryption_key = "Customer Managed"
+        if default_kms_key_name: 
+            encryption_key = 'Customer Managed'
+            key_location = parse_key(default_kms_key_name)
         else:
-            encryption_key = "Google Managed"
+            encryption_key = 'Google Managed'
+            key_location = location
 
         table.add_row(
             bucket,
             encryption_algorithm,
-            encryption_key
+            encryption_key,
+            f'{key_location}: ✅' if key_location.startswith('europe-') else '❌',
+            f'{location}: ✅' if location.startswith('europe-') else '❌'
         )
         
     console.print(table)
